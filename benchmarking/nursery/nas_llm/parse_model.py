25f1610b14592b8b24482731565bcfad1a579c0f
aaronkl <kleinaa@cs.uni-freiburg.de>
2023-05-23 13:31:02 +0200

    obtain final model

---
+++ benchmarking/nursery/nas_llm/parse_model.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+import torch.nn as nn
+
+
+def get_final_model(original_model, new_model_config):
+
+    new_model = AutoModelForSequenceClassification.from_config(new_model_config)
+
+    for name, module in new_model.bert.named_modules():
+        if isinstance(module, nn.Linear):
+
+            original_layer = original_model.bert.get_submodule(name).state_dict()
+
+            weight_shape = module.state_dict()['weight'].shape
+            bias_shape = module.state_dict()['bias'].shape
+
+            new_state_dict = OrderedDict()
+            new_state_dict['weight'] = original_layer['weight'][:weight_shape[0], :weight_shape[1]]
+            new_state_dict['bias'] = original_layer['bias'][:bias_shape[0]]
+            module.load_state_dict(new_state_dict)
+
+    new_model.bert.embeddings = model.bert.embeddings
+    new_model.bert.pooler = model.bert.pooler
+    new_model.classifier = model.classifier
+
+    return new_model
+
+
+if __name__ == "__main__":
+    from transformers import AutoModelForSequenceClassification
+    model_type = 'bert-base-cased'
+    model = AutoModelForSequenceClassification.from_pretrained(model_type)
+
+    from transformers.models.bert.modeling_bert import BertConfig
+    config = BertConfig(num_hidden_layers=6, num_attention_heads=6, intermediate_size=1024)
+    new_model = get_final_model(original_model=model, new_model_config=config)
