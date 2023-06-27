72e807861b42f32111faeebc0021f51a83430a5e
aaronkl <kleinaa@cs.uni-freiburg.de>
2023-06-27 09:52:53 +0200

    add draft notebook

---
+++ benchmarking/nursery/nas_amt/mask/__init__.py
@@ -0,0 +1,3 @@
+from .mask_gpt import mask_gpt
+from .mask_bert import mask_bert
+from .mask_gptneox import mask_gpt_neox
---
+++ benchmarking/nursery/nas_amt/mask/mask_gpt.py
@@ -0,0 +1,62 @@
+from .utils import register_mask_ffn, get_backbone, \
+    register_drop_layer, register_drop_attention_layer
+
+
+def get_ffn2(model, index):
+    layer = get_layers(model)[index]
+    ffn2 = layer.mlp.c_proj
+    return ffn2
+
+
+def get_mlp(model, index):
+    layer = get_layers(model)[index]
+    return layer.mlp
+
+
+def get_attention_output(model, index):
+    layer = get_layers(model)[index]
+    output = layer.attn
+    return output
+
+
+def get_layers(model):
+    decoder = get_backbone(model)
+    layers = decoder.h
+    return layers
+
+
+def register_mask(module, mask):
+    hook = lambda _, inputs: (inputs[0] * mask)
+    handle = module.register_forward_pre_hook(hook)
+    return handle
+
+
+def register_drop_mlp_layer(module):
+    hook = lambda _, input, output: input[0]
+    handle = module.register_forward_hook(hook)
+    return handle
+
+
+def mask_gpt(model, neuron_mask, head_mask):
+    num_hidden_layers = neuron_mask.shape[0]
+
+    assert head_mask.shape[0] == num_hidden_layers
+
+    handles = []
+    for layer_idx in range(num_hidden_layers):
+        ffn2 = get_ffn2(model, layer_idx)
+        handle = register_mask(ffn2, neuron_mask[layer_idx])
+        handles.append(handle)
+
+        if neuron_mask[layer_idx].sum() == 0:
+            mlp = get_mlp(model, layer_idx)
+            handle = register_drop_mlp_layer(mlp)
+            handles.append(handle)
+
+        if head_mask[layer_idx].sum() == 0:
+            attention = get_attention_output(model, layer_idx)
+            handle = register_drop_attention_layer(attention)
+
+            handles.append(handle)
+
+    return handles
